03842cf5237d3b701230e2547e6b993e4600fa52
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-10-21 14:38:48 +0100

    Added section for total amount of money

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 console.log("Financial Analysis");
 console.log("----------------");
 console.log("Total Months: " + finances.length);
+
+let total = 0;
+
+for (let i=0; i < finances.length; i++) {
+    total = total + finances[i][1];
+}
+
+console.log("Total: $" + total);
