fbc47702722914330d671ff2b254f7a30d616c92
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-10-21 17:06:05 +0100

    Added section which works out the average change

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ for (let i=0; i < finances.length; i++) {
 }
 
 console.log("Total: $" + total);
+
+let arrayOfMoney = [];
+for (let i=0; i < finances.length - 1; i++) {
+    arrayOfMoney.push(finances[i+1][1] - finances[i][1])
+}
+
+let totalAmountOfMoney = 0;
+for (let i=0; i < arrayOfMoney.length; i++) {
+    totalAmountOfMoney = totalAmountOfMoney + arrayOfMoney[i];
+}
+totalAmountOfMoney = totalAmountOfMoney / (finances.length - 1);
+totalAmountOfMoney = totalAmountOfMoney.toFixed(2);
+console.log("Average Change: " + totalAmountOfMoney);
