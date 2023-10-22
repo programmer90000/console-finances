1372e0579c6fd9cf7b5b77bc27d89a945f21e96f
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-10-22 10:53:41 +0100

    Added section to log greatest profit increase/ decrease

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ for (let i=0; i < arrayOfMoney.length; i++) {
 average_change = average_change / (finances.length - 1);
 average_change = average_change.toFixed(2);
 console.log("Average Change: " + average_change);
+
+let greatest_increase = 0;
+let greatest_decrease = 0;
+let month_of_greatest_increase;
+let month_of_greatest_decrease;
+
+for (let i=0; i < finances.length - 1; i++) {
+    if (finances[i+1][1] - finances[i][1] > greatest_increase) {
+        greatest_increase = finances[i+1][1] - finances[i][1];
+        month_of_greatest_increase = finances[i+1][0];
+    }
+    if (finances[i+1][1] - finances[i][1] < greatest_decrease) {
+        greatest_decrease = finances[i+1][1] - finances[i][1];
+        month_of_greatest_decrease = finances[i+1][0];
+    }
+}
+
+console.log('Greatest Increase in Profits/Losses: ' + month_of_greatest_increase + ' ($' + greatest_increase + ')');
+console.log('Greatest Decrease in Profits/Losses: ' + month_of_greatest_decrease + ' ($' + greatest_decrease + ')');
