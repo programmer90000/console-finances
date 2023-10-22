7e63d84783d33870ef67a5bda05c557066c68774
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-10-22 11:06:53 +0100

    Added space in the file between each section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Financial Analysis");
 console.log("----------------");
-console.log("Total Months: " + finances.length);
+console.log("Total Months: " + finances.length); // Log length of finances Array in index.js
+
 
 let total = 0;
 
@@ -10,18 +11,24 @@ for (let i=0; i < finances.length; i++) {
 
 console.log("Total: $" + total);
 
+
 let arrayOfMoney = [];
+
 for (let i=0; i < finances.length - 1; i++) {
     arrayOfMoney.push(finances[i+1][1] - finances[i][1])
 }
 
 let average_change = 0;
+
 for (let i=0; i < arrayOfMoney.length; i++) {
     average_change = average_change + arrayOfMoney[i];
 }
+
 average_change = average_change / (finances.length - 1);
 average_change = average_change.toFixed(2);
+
 console.log("Average Change: " + average_change);
+
 
 let greatest_increase = 0;
 let greatest_decrease = 0;
